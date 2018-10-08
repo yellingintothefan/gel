9df9fa662e47658f960296d194ccba2c7a3afb2d
Mathias Rav <m@git.strova.dk>
2018-10-08 16:11:56 +0200

    Add script to download and extract model zip files

---
+++ dl.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+python3 dl.py https://www.models-resource.com/download/593/ salesman
+python3 dl.py https://www.models-resource.com/download/4909/ dk
+python3 dl.py https://www.models-resource.com/download/677/ gohma
+python3 dl.py https://www.models-resource.com/download/4956/ charizard
+python3 dl.py https://www.models-resource.com/download/470/ saria
+python3 dl.py https://www.models-resource.com/download/689/ sheik
